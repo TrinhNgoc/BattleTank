054197ca9372918035a5897a51430900afec35a0
Jenny <jennyngu@hawaii.edu>
2018-09-08 22:26:38 -1000

    Create Default Sub Objects in C++

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -28,7 +29,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 	
 	
